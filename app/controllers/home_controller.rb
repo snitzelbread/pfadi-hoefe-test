77da78bbec56d9958d4999dfef7fec0dbe9ae610
Julian Duss <julianduss@Julians-MacBook-Very-Pro.local>
2024-09-13 12:15:50 +0200

    Hocks on home page are now the closest one to the current date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,7 @@
 class HomeController < ApplicationController
       def index
-        @biber_hocks = Hock.where(stufe: "Biber").limit(3)
-        @woelfli_hocks = Hock.where(stufe: "Wölfli").limit(3)
-        @pfadi_hocks = Hock.where(stufe: "Pfadi").limit(3)
-
-        # Debugging: Print out the results to ensure they are set
-        Rails.logger.debug "Biber Hocks: #{@biber_hocks.inspect}"
-        Rails.logger.debug "Woelfli Hocks: #{@woelfli_hocks.inspect}"
-        Rails.logger.debug "Pfadi Hocks: #{@pfadi_hocks.inspect}"
+        @closest_biber_hocks = Hock.where("stufe = ? AND datetime > ?", "Biber", Time.zone.now).order(:datetime).first
+        @closest_woelfli_hocks = Hock.where("stufe = ? AND datetime > ?", "Wölfli", Time.zone.now).order(:datetime).first
+        @closest_pfadi_hocks = Hock.where("stufe = ? AND datetime > ?", "Pfadi", Time.zone.now).order(:datetime).first
       end
 end

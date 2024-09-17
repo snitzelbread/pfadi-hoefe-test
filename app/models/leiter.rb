dfc370d7a3b2853def89edec01ce104a208d1886
Julian Duss <julianduss@Julians-MacBook-Very-Pro.local>
2024-09-17 17:48:40 +0200

    Added partial uniqueness to Stufe and Funktion, only one (Stv.) Abteilungsleitung allowed and only one Stufenleiter per Stufe

--- app/models/leiter.rb
+++ app/models/leiter.rb
@@ -6,5 +6,19 @@ class Leiter < ApplicationRecord
   validates :pfadiname, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
-  validates :stufe, uniqueness: { scope: :funktion, message: "und Funktion-Kombination ist bereits in Verwendung." }
+  validate :unique_stufenleitung_for_stufe, if: -> { funktion == 'Stufenleitung' }
+  validate :unique_global_funktionen, if: -> { ['Stv. Abteilungsleitung', 'Abteilungsleitung'].include?(funktion) }
+
+  private
+
+  def unique_global_funktionen
+    if Leiter.where(funktion: funktion).where.not(id: id).exists?
+      errors.add(:funktion, "#{funktion} kann es nur einmal geben.")
+    end
+  end
+  def unique_stufenleitung_for_stufe
+    if Leiter.where(funktion: 'Stufenleitung', stufe: stufe).where.not(id: id).exists?
+      errors.add(:funktion, "Stufenleitung can only be assigned to one Leiter per stufe")
+    end
+  end
 end

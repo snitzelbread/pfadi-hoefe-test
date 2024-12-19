a5cbee7936d9c98b10a76b7635ca5f6924e6496b
julianduss <julian.duss@renuo.ch>
2024-12-19 20:14:37 +0100

    CSS for ranged events and some minor bug fixes for leaders

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -50,6 +50,46 @@ module CalendarHelper
     events
   end
 
+  def start_middle_or_end(event, date)
+    if event.start_time.month == date.month && event.start_time.day == date.day
+      "start"
+    elsif event.end_time.month == date.month && event.end_time.day == date.day
+      "end"
+    else
+      "middle"
+    end
+  end
+
+  def ranged_events(events)
+    ranged_events = events.select { |event| event.start_time.month != event.end_time.month || event.start_time.day != event.end_time.day }
+    ranged_events.each do |event|
+      if event.end_time.hour == 0
+        event.end_time -= 1.minute
+      end
+    end
+  end
+
+  def single_day_events(events)
+
+    single_day_events = events.select { |event| event.start_time.month == event.end_time.month && (
+      event.start_time.day == event.end_time.day || end_time(event)
+    )
+
+    }
+  end
+
+  def end_time(event)
+    if event.end_time.hour == 0
+      end_time = event.end_time
+      end_time -= 1.minute
+      if end_time.day == event.end_time.day
+        return false
+      else
+        return true
+      end
+    end
+  end
+
   def events_on_day_and_month(date, calendars)
     events = []
     calendars.each do |cal|
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,6 +9,19 @@ import "controllers"
 import "trix"
 import "@rails/actiontext"
 
+Turbo.setConfirmMethod((message, element) =>{
+    console.log(message, element)
+    let dialog = document.getElementById("turbo-confirm")
+    dialog.querySelector("p").textContent = message
+    dialog.showModal()
+
+    return new Promise((resolve, reject) => {
+        dialog.addEventListener("close", () => {
+            resolve(dialog.returnValue === "confirm")
+        }, {once: true})
+    })
+})
+
 function initializePopovers() {
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))

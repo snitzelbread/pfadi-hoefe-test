1a71c5bad446719146342f3b78dbfdc56d8e6ae5
snitzelbread <julian.duss@renuo.ch>
2025-02-18 17:14:15 +0100

    Overhaul font sizes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,9 @@
 class HomeController < ApplicationController
-  include CalendarHelper
-  # before_action :detect_device
-
   def index
     @closest_biber_hock = Hock.closest_hock_by_stufe("Biber").first
     @closest_woelfli_hock = Hock.closest_hock_by_stufe("Wölfli").first
     @closest_pfadi_hock = Hock.closest_hock_by_stufe("Pfadi").first
 
     @latest_article = Article.latest_articles.first
-
-    @home_calendars = calendar_with_these_names(Calendar::HOME_CALENDARS)
   end
 end
